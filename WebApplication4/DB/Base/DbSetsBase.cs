842ccc4446b7e14f4a90b55281b77db93d7dd314
TheCrazyWolf <alex_kulagin@samgk.ru>
2023-11-15 19:16:30 +0400

    Выносим в базовый класс все таблицы

---
+++ WebApplication4/DB/Base/DbSetsBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KipperPro.API_ASP_CORE.DB.Base;
+
+public class DbSetsBase : DbContext
+{
+    public DbSet<AttendsGroups> AttendsGroups { get; set; }
+    public DbSet<AttendsPersonally> AttendsPersonally { get; set;}
+    public DbSet<Department> Department { get; set; }
+    public DbSet<Division> Divisions { get; set; }
+    public DbSet<User> Users { get; set; }   
+    public DbSet<Visitor> Visitors { get; set; }
+}
--- WebApplication4/DB/MsContext.cs
+++ WebApplication4/DB/MsContext.cs
@@ -14,12 +14,5 @@ namespace KipperPro.API_ASP_CORE.DB
             optionsBuilder.UseSqlServer(_config["connectionStringSql"]);
         }
 
-        public DbSet<AttendsGroups> AttendsGroups { get; set; }
-        public DbSet<AttendsPersonally> AttendsPersonally { get; set;}
-        public DbSet<Department> Department { get; set; }
-        public DbSet<Division> Divisions { get; set; }
-        public DbSet<User> Users { get; set; }   
-        public DbSet<Visitor> Visitors { get; set; }
-
     }
 }
