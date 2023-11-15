aaad193d04511f349e94f2d98020ab078dc52af5
TheCrazyWolf <alex_kulagin@samgk.ru>
2023-11-15 18:47:21 +0400

    Добавление зависимостей в контроллер

--- WebApplication4/Controllers/RegistrationController.cs
+++ WebApplication4/Controllers/RegistrationController.cs
@@ -1,6 +1,7 @@
 ﻿using KipperPro.API_ASP_CORE.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using WebApplication4.ModelApi;
 
 namespace WebApplication4.Controllers
@@ -9,7 +10,12 @@ namespace WebApplication4.Controllers
     [ApiController]
     public class RegistrationController : ControllerBase
     {
-
+        private readonly ValidationService _validation;
+        public RegistrationController(ValidationService validation)
+        {
+            _validation = validation;
+        }
+        
         [HttpPost("registration")]
         public ActionResult<bool> Registration(UserRequest request)
         {
