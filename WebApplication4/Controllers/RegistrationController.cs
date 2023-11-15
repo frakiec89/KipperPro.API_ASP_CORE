3e495fe83a8d020a0a64f534450cc4d94045a27b
TheCrazyWolf <alex_kulagin@samgk.ru>
2023-11-15 18:50:10 +0400

    Используем нашу валидацию из сервиса

--- WebApplication4/Controllers/RegistrationController.cs
+++ WebApplication4/Controllers/RegistrationController.cs
@@ -11,64 +11,43 @@ namespace WebApplication4.Controllers
     public class RegistrationController : ControllerBase
     {
         private readonly ValidationService _validation;
+
         public RegistrationController(ValidationService validation)
         {
             _validation = validation;
         }
-        
+
         [HttpPost("registration")]
         public IActionResult Registration(UserRequest request)
         {
-            if (request == null)
-            {  return BadRequest("пустой запрос");            }
-
-            if (string.IsNullOrWhiteSpace(request.Password))
-            {return BadRequest("пустой пароль");            }
-
-            if (string.IsNullOrWhiteSpace(request.Name))
-            { return BadRequest("пустое имя"); }
-
-            if (string.IsNullOrWhiteSpace(request.Email))
-            { return  BadRequest("Email логин"); }
-
-
-            if (request.Password.Length < 8)
-                return BadRequest("пароль короткий");
-
-            if (request.Password.ToLower() == request.Password ||
-                request.Password.ToLower() == request.Password
-                )
-                return BadRequest("пароль плохой");
-            // todo - куча  проверок 
+            if (_validation.IsValidRegistraion(request))
+                return BadRequest(
+                    "Запрос не прошел валидацию. Логин либо пустой, пароль возможно тоже, пароль должен быть не меньше 6 символов");
 
             try
             {
                 UsersService service = new UsersService();
-                service.AddUser (request);
+                service.AddUser(request);
             }
             catch (Exception ex)
             {
                 BadRequest(ex.Message);
             }
-            return true ;
+
+            return true;
         }
 
         [HttpPost("authUser")]
-        public IActionResult AuthUser (UserRequest request)
+        public IActionResult AuthUser(UserRequest request)
         {
-            if (request == null)
-            { return BadRequest("пустой запрос"); }
-
-            if (string.IsNullOrWhiteSpace(request.Password))
-            { return BadRequest("пустой пароль"); }
-
-            if (string.IsNullOrWhiteSpace(request.Email))
-            { return BadRequest("пустой логин"); }
+            if (_validation.IsValidLogin(request))
+                return BadRequest(
+                    "Запрос не прошел валидацию. Логин либо пустой, пароль возможно тоже");
 
             try
             {
                 UsersService service = new UsersService();
-                UserResponse us = service.GetUser (request);
+                UserResponse us = service.GetUser(request);
 
                 return us;
             }
@@ -76,8 +55,6 @@ namespace WebApplication4.Controllers
             {
                 return BadRequest(ex.Message);
             }
-
-
         }
     }
 }
