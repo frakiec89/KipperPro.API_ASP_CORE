1b106de74c64cd4f294a79006e384f85c284625a
TheCrazyWolf <alex_kulagin@samgk.ru>
2023-11-15 18:55:12 +0400

    Убираем создание EF экземпляра, делаем из него сервис

--- WebApplication4/Services/UsersService.cs
+++ WebApplication4/Services/UsersService.cs
@@ -1,37 +1,42 @@
-﻿using WebApplication4.ModelApi;
+﻿using KipperPro.API_ASP_CORE.DB;
+using WebApplication4.ModelApi;
 
-namespace KipperPro.API_ASP_CORE.Services
+namespace KipperPro.API_ASP_CORE.Services;
+
+internal class UsersService
 {
-    internal class UsersService
+    private MsContext _context;
+    public UsersService(MsContext context)
     {
-        DB.MsContext context = new DB.MsContext();
-        internal void AddUser(UserRequest request)
-        {
-            try
-            {
-                var us = new DB.Visitor(request.Name, request.Password, request.Email);
-                context.Visitors.Add(us);
-                context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                throw new Exception( e.Message);
-            }
-        }
+        _context = context;
+    }
 
-        internal UserResponse GetUser(UserRequest request)
+    public void AddUser(UserRequest request)
+    {
+        try
         {
-            try
-            {
-                var us = 
-                context.Visitors.Single(x => x.Email == request.Email
-                && x.Password == request.Password) ;
-                return new UserResponse { Id = us.VisitorId , Name = us.Name };
-            }
-            catch (Exception e)
-            {
-                throw new Exception(e.Message);
-            }
+            var us = new DB.Visitor(request.Name, request.Password, request.Email);
+            _context.Visitors.Add(us);
+            _context.SaveChanges();
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
+        }
+    }
+
+    public UserResponse GetUser(UserRequest request)
+    {
+        try
+        {
+            var us =
+                _context.Visitors.Single(x => x.Email == request.Email
+                                              && x.Password == request.Password);
+            return new UserResponse { Id = us.VisitorId, Name = us.Name };
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message);
         }
     }
 }
